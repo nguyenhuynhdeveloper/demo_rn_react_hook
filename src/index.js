6d39b0e481b486e29aa9d1d662d031dc4f94762e
nguyenhuynhdeveloper <nguyenhuynhdeveloper>
2023-09-28 21:54:54 +0700

    change folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export{default as Hook_useState} from './Hook_useState'
-export {default as Hook_useEffect} from './Hook_useEffect'
-export {default as Hook_useRef} from './Hook_useRef'
-export {default as Hook_useCallback}  from './Hook_useCallback'
-export {default as Hook_useContext}  from './Hook_useContext'
-export {default as Hook_useMemo} from './Hook_useMemo'
-export {default as Hook_useReducer } from './Hook_useReducer'
-export {default as React_memo} from './React_memo'
+export {default as Hook_useState} from './doc_useState/Hook_useState';
+export {default as Hook_useEffect} from './doc_useEffect/Hook_useEffect';
+export {default as Hook_useRef} from './doc_useRef/Hook_useRef';
+export {default as Hook_useCallback} from './doc_useCallback/Hook_useCallback';
+export {default as Hook_useContext} from './doc_useContext/Hook_useContext';
+export {default as Hook_useMemo} from './doc_useMemo/Hook_useMemo';
+export {default as Hook_useReducer} from './doc_useReducer/Hook_useReducer';
+export {default as React_memo} from './doc_memo/React_memo';
